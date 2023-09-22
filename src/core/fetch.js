60062b06c003ad81d28f12f319b8ecfbc91c6610
zhenhu.li <2483433169@qq.com>
2023-09-22 11:05:30 +0800

    fix: 修复相关保险商品报价为0时不能自动从购物车移除的问题

--- src/core/fetch.js
+++ src/core/fetch.js
@@ -184,7 +184,7 @@ export const getQuoteResults = async (shop, cart, types) => {
     const quotes = await Promise.all(promises);
     return quotes;
   } catch (e) {
-    console.log(e.message);
+    console.log("getQuoteResults",e.message);
     return null;
   }
 };
