812c2d8b0dd1f28ef5b4041d9db361800a2fcee9
liuliu hu <liuliu@seel.com>
2023-11-28 14:36:58 +0800

    feat(aeb9ab.myshopify.com has-no-bounds.myshopify.com 0f19ac-2.myshopify.com): add three shop

---
+++ src/index.aeb9ab.myshopify.com.js
@@ -0,0 +1,129 @@
+import initialize, { seelEvents } from "./core";
+import store, { snapshot } from "./core/store";
+import embedWidget, {
+  flatten as repaint,
+} from "./component/cart-widget/index.needInitAddWidget";
+import configurations from "./config/aeb9ab.myshopify.com.json";
+import renderModal from "./component/modal";
+import renderPdpBanner from "./component/pdp-banner";
+//import configurations from "./config/index.json";
+import { productType } from "./core/constant";
+import "./component/cart-widget/aeb9ab.myshopify.com.css";
+import { rerenderCart } from "./core/util";
+// get myshopify domain from global var
+
+store.configs = configurations;
+
+const shop = window?.Shopify?.shop || window?.Shopify?.Checkout?.apiHost;
+
+const subtotalSelector = "";
+const dynamicSubtotalSelector = "#CartDrawer .drawer__footer .ajaxcart__price";
+const chekoutBtnSelector = "#MainContent [name=checkout]";
+const dynamicCheckoutBtnSelector = "#CartDrawer [name=checkout]";
+const dynamicUpdateSection = "";
+const updateSection = "";
+
+const changeSubtotal = (snapshot) => {
+  // Change Subtotal
+  if (!snapshot.quotes || !snapshot.quotes.length) {
+    return;
+  }
+  const { currencySymbol, currencyCode } = snapshot.quotes[0] || {};
+  const { total_price: amount } = snapshot.cart;
+  const subTotal = `${currencySymbol} ${(amount / 100).toFixed(2)} ${
+    currencyCode || ""
+  }`;
+
+  if (subtotalSelector && document.querySelector(subtotalSelector)) {
+    const element = document.querySelector(subtotalSelector);
+    element.innerHTML = subTotal;
+  }
+  if (
+    dynamicSubtotalSelector &&
+    document.querySelector(dynamicSubtotalSelector)
+  ) {
+    const element = document.querySelector(dynamicSubtotalSelector);
+    element.innerHTML = subTotal;
+  }
+};
+
+(async () => {
+  await initialize(shop);
+
+  renderPdpBanner(productType.ra, shop);
+
+  // Discard the first cart_updated event, and manually complete the inital rendering
+  changeSubtotal(snapshot(store));
+  store?.types?.forEach?.((type) => {
+    embedWidget(type);
+    renderModal(type);
+  });
+
+  // Cart Update Handler
+  document.addEventListener(seelEvents.cartUpdated, () => {
+    // Rerender cart
+    try {
+      rerenderCart(updateSection, dynamicUpdateSection, store);
+    } catch {
+      console.log("rerender cart fail");
+    }
+    // Rerender widget
+    store?.types?.forEach?.((type) => {
+      const widget = document.querySelector(
+        `.seel_widget[data-seel-product-type='${type}']`,
+      );
+      if (widget) {
+        console.log(`repaint ${type}`);
+        repaint(widget, type);
+      } else {
+        console.log(`reembed ${type}`);
+        embedWidget(type);
+      }
+    });
+    // Remove widget
+    Object.values(productType).forEach((type) => {
+      const found = store.types.find((_) => _ === type);
+      const widget = document.querySelector(
+        `.seel_widget[data-seel-product-type='${type}']`,
+      );
+      if (!found && widget) {
+        widget.remove();
+      }
+    });
+
+    // Change Subtotal
+    changeSubtotal(snapshot(store));
+    chekoutBtnSelector &&
+      document
+        .querySelector(chekoutBtnSelector)
+        ?.addEventListener("click", submitHandler);
+    dynamicCheckoutBtnSelector &&
+      document
+        .querySelector(dynamicCheckoutBtnSelector)
+        ?.addEventListener("click", submitHandler);
+  });
+
+  // Change default click behavior of checkout button
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    // const { url: checkoutUrl } = await fetch(`${window.location.origin}/cart`, {
+    //   headers: {
+    //     "content-type": "application/x-www-form-urlencoded",
+    //   },
+    //   method: "post",
+    //   body: "checkout=",
+    // });
+    // window.open(checkoutUrl, "_self");
+    window.open("/checkout", "_self");
+    // return false;
+  };
+  // Bind event
+  chekoutBtnSelector &&
+    document
+      .querySelector(chekoutBtnSelector)
+      ?.addEventListener("click", submitHandler);
+  dynamicCheckoutBtnSelector &&
+    document
+      .querySelector(dynamicCheckoutBtnSelector)
+      ?.addEventListener("click", submitHandler);
+})();
