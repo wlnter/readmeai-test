68b89d5c3e9c5b15d12c83af2b4d7bcdebda0905
winter <winter.ld@outlook.com>
2023-11-30 22:31:43 +0800

    chore: 增加override功能，方便shopify-script迁移过程中的调试

--- mock.mjs
+++ mock.mjs
@@ -7,6 +7,11 @@ const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 const mockDir = "./mock/static.seel.com/shopify";
 const testMockDir = "./mock/static-test.seel.com/shopify";
+const overrideRADir = "./mock/cdn.seel.com/releases/scripts/shopify-ra";
+const override17BPDir = "./mock/cdn.seel.com/releases/scripts/shopify-bp";
+
+fs.mkdirSync(overrideRADir, { recursive: true });
+fs.mkdirSync(override17BPDir, { recursive: true });
 
 fs.cpSync("dist", mockDir, { recursive: true });
 fs.cpSync("dist", testMockDir, { recursive: true });
@@ -29,4 +34,29 @@ glob.sync(`${mockDir}/script/*.js`).forEach((scriptPath) => {
 });
 glob.sync(`${testMockDir}/script/*.js`).forEach((scriptPath) => {
   copyScript(testMockDir, scriptPath);
+});
+
+glob.sync(`./dist/script/*.js`).forEach((scriptPath) => {
+  const dir = scriptPath.split("/");
+  const name = dir[dir.length - 1];
+  const temp = name.split(".");
+  const integrationPoint = temp.shift();
+  temp.pop();
+  const shop = temp.join(".");
+  if (integrationPoint === "index" && shop) {
+    fs.copyFileSync(
+      path.join(__dirname, `./dist/script/${name}`),
+      path.join(
+        __dirname,
+        `${overrideRADir}/return-assurance.min.js?shop=${shop}`,
+      ),
+    );
+    fs.copyFileSync(
+      path.join(__dirname, `./dist/script/${name}`),
+      path.join(
+        __dirname,
+        `${override17BPDir}/bp-embedded.min.js?shop=${shop}`,
+      ),
+    );
+  }
 });
