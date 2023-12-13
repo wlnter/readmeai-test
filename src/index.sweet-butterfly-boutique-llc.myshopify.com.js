2c377dee257e0baf0747f0253fb9512c7dff7a36
liuliu hu <liuliu@seel.com>
2023-12-13 17:44:32 +0800

    fix(sweet-butterfly-boutique-llc.myshopify.com): fixed 侧边栏ra弹窗被遮挡

--- src/index.sweet-butterfly-boutique-llc.myshopify.com.js
+++ src/index.sweet-butterfly-boutique-llc.myshopify.com.js
@@ -13,6 +13,7 @@ import embedPdpWidget, {
   flatten as repaintPdpWidget,
 } from "./component/pdp-widget";
 import { scriptingMarker } from "./pixel/performance.js";
+import "./component/cart-widget/sweet-butterfly-boutique-llc.myshopify.com.css";
 
 store.configs = configurations;
 
